788dd2fe76203bb9280aa5dd8a4c206d7df6bc63
Ed <datum1337@gmail.com>
2019-03-29 15:32:33 +0000

    base

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+var globalResults;
+
+mongoClient.connect(url, function(err, db){
+  if(err) throw err;
+  var dbo = db.db("mydb");
+  var query = {_id: 0, name: 1, img: 1, desc: 1, desclong: 1};
+  dbo.collection("customers").find({},{projection: query}).toArray(function(err, result){
+    if(err) throw err;
+    globalResults = result;
+    console.log(result);
+    db.close();
+  });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { result: globalResults });
+});
+
+router.post("/bees",function(req, res){
+  console.log(req);
+  console.log(req.body.name);
+  console.log(req.body);
+  res.render("index", {result: globalResults, type: req.body, bee: true});
+});
+
+router.get("/bees" , function(req, res){
+  res.render("index", {result: globalResults, type:req.query, bee: true});
+});
+
+module.exports = router;
