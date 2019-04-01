382872e5dd001b758487558987f63740f443dc9b
Ed <datum1337@gmail.com>
2019-04-01 18:11:46 +0100

    socketio working :) still need to integrate comments

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on("disconnect", function(){
+    console.log("user disconnected");
+  });
+  socket.on("chat message", function(msg){
+    console.log("message: " + msg);
+  });
 });
 
 app.use(function(req, res, next){
