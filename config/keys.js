1a41b5118b3210146bf64ae85cd0fa4cd5c578c6
KingKoopasGoomba <lucasdan7276@gmail.com>
2019-04-02 19:10:28 +0100

    added dynamic categories

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,8 @@
 module.exports = {
-    MongoURI: "mongodb://localhost:27017/test",
+    MongoURI: "mongodb://localhost:27017/BananaJuice",
     videoFormats: [
         "video/mp4",
         "video/webm",
         "video/ogg" ,
-
     ],
-    videoCategories: [
-        "Cat Videos",
-        "Not Cat Videos"
-    ]
 };
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const Videos = require('../models/Video');
-const categories = require('../config/keys').videoCategories;
+const Categories  = require('../models/Category')
+
 //const { ensureAuthenticated } = require('../config/auth');
 
 /* GET home page. */
 router.get('/', (req, res) => {
     Videos.find({})
         .then(videos =>{
-            res.render('welcome', {categories: categories, videos: videos})
+            Categories.find({})
+                .then(categories => {
+                    console.log(categories);
+                    res.render('welcome', {categories: categories , videos: videos})
+                })
         })
 });
 
 router.get('/dashboard', (req, res) => {
     if (req.user) {
-        res.render('user/dashboard', {user: req.user.name})
+        res.render('user/dashboard', {user: req.user.name});
     } else {
         req.flash('error_msg','please login first');
-        res.redirect('/users/login')
+        res.redirect('/users/login');
+
     }
 });
 
